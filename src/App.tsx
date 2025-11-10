dd324d57975539e774ca0aab738292ea2d1a880e
Fawzy Hany <fawzyshoman1710@gmail.com>
2025-11-10 19:43:49 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+
+
+export default function App() {
+
+
+  return (
+    <>
+   <h1>Hello, This is Safarni Project</h1>
+   <Outlet />
+    </>
+  )
+}
+
+
